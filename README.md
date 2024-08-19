b6680f61ab587f0a8cf38d42f27552b4772dc1fb
Ariadna Ardere <arxofari@proton.me>
2024-08-19 19:18:10 +0200

    pushed with lzps

---
+++ README.md
@@ -0,0 +1,15 @@
+### *The lazy **and** easiest way to push code to github*
+
+##### arguments
+-f | same as '-f'
+-fwl | same as '--force-with-lease'
+
+##### Important info
+lzps automatically uses 'main' as it's push branch, you can change this in the script if you wish to
+
+
+####### ok but what about lazygi-
+shut.
+lazygit isn't lazy
+it's a TUI that you have to navigate
+lzps is meant to be fast, minimal and super easy and lazy to use, and that's what it is
--- lzps.sh
+++ lzps.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+usage() {
+    echo "Usage: $0 [-f | -fwl]"
+    echo "  -f    Force push (git push -f)"
+    echo "  -fwl  Force push with lease (git push --force-with-lease)"
+    exit 1
+}
+
+if [ "$#" -gt 1 ]; then
+    usage
+fi
+
+case "$1" in
+    -f)
+        push_option="--force"
+        ;;
+    -fwl)
+        push_option="--force-with-lease"
+        ;;
+    "")
+        push_option=""
+        ;;
+    *)
+        usage
+        ;;
+esac
+
 read -p "Enter commit message (or press Enter to use default): " commit_message
 
 if [ -z "$commit_message" ]; then
@@ -7,7 +33,10 @@ if [ -z "$commit_message" ]; then
 fi
 
 git add .
-
 git commit -m "$commit_message"
 
-git push origin main
+if [ -z "$push_option" ]; then
+    git push origin main
+else
+    git push $push_option origin main
+fi
